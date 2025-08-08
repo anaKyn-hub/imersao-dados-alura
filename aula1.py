84413be5d035c6852279aad2437639f20c202a99
anaKyn <anakyndeveloper@gmail.com>
2025-08-07 22:32:48 -0300

    começo aula 1

--- aula1.py
+++ aula1.py
@@ -1,3 +1,9 @@
 import pandas as pd
+import pandas_config
 
 df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+
+print(df.head(7)) 
+print(df.info())
+print(df.describe())
+print(df.shape) #não tem parenteses porque é atributo, não método, sua saída mostra o número de linhas em seguida o número de colunas
